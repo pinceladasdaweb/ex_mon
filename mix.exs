02d46fcfae1218ef9555e64a6daefbcf023120e7
Pedro Rogério <pinceladasdaweb@hotmail.com>
2020-10-07 21:34:33 -0300

    Added Argon2

--- mix.exs
+++ mix.exs
@@ -42,7 +42,8 @@ defmodule ExMon.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:argon2_elixir, "~> 2.3"}
     ]
   end
 
