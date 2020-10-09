dd03524d8c3332fb7ed31e4a006f97b69b793869
Pedro Rogério <pinceladasdaweb@hotmail.com>
2020-10-09 20:52:38 -0300

    Update facades

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,5 +1,8 @@
 defmodule ExMon do
   alias ExMon.Trainer
 
+  defdelegate fetch_trainer(params), to: Trainer.Get, as: :call
   defdelegate create_trainer(params), to: Trainer.Create, as: :call
+  defdelegate update_trainer(params), to: Trainer.Update, as: :call
+  defdelegate delete_trainer(params), to: Trainer.Delete, as: :call
 end
