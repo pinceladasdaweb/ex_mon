976a3b538fcbc313e499af5dbf763c1b6e127513
Pedro Rogério <pinceladasdaweb@hotmail.com>
2020-10-09 20:53:33 -0300

    Update trainers controller

--- lib/ex_mon_web/controllers/trainers_controller.ex
+++ lib/ex_mon_web/controllers/trainers_controller.ex
@@ -3,17 +3,43 @@ defmodule ExMonWeb.TrainersController do
 
   action_fallback ExMonWeb.FallbackController
 
+  def show(conn, %{"id" => id}) do
+    id
+    |> ExMon.fetch_trainer()
+    |> handle_response(conn, "show.json", :ok)
+  end
+
   def create(conn, params) do
-  	params
-  	|> ExMon.create_trainer()
-  	|> handle_response(conn)
+    params
+    |> ExMon.create_trainer()
+    |> handle_response(conn, "create.json", :created)
   end
 
-  defp handle_response({:ok, trainer}, conn) do
-  	conn
-  	|> put_status(:ok)
-  	|> render("create.json", trainer: trainer)
+  def update(conn, params) do
+    params
+    |> ExMon.create_trainer()
+    |> handle_response(conn, "update.json", :ok)
   end
 
-  defp handle_response({:error, _changeset} = error, _conn), do: error
+  def delete(conn, %{"id" => id}) do
+    id
+    |> ExMon.delete_trainer()
+    |> handle_delete(conn)
+  end
+
+  defp handle_response({:ok, trainer}, conn, view, status) do
+    conn
+    |> put_status(status)
+    |> render(view, trainer: trainer)
+  end
+
+  defp handle_response({:error, _changeset} = error, _conn, _view, _status), do: error
+
+  defp handle_delete({:ok, _trainer}, conn) do
+    conn
+    |> put_status(:no_content)
+    |> text("")
+  end
+
+  defp handle_delete({:error, _reason} = error, _conn), do: error
 end
