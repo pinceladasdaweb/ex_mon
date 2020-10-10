f53f63736c19e83f70908a5085cfa336470fc1d5
Pedro Rogério <pinceladasdaweb@hotmail.com>
2020-10-09 21:03:02 -0300

    Added Tesla HTTP client

--- mix.exs
+++ mix.exs
@@ -43,7 +43,8 @@ defmodule ExMon.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:argon2_elixir, "~> 2.3"}
+      {:argon2_elixir, "~> 2.3"},
+      {:tesla, "~> 1.3.3"}
     ]
   end
 
