a7ee0657998d2cc8082486cff12e06f1ea8efd64
Pedro Rogério <pinceladasdaweb@hotmail.com>
2020-10-14 20:38:16 -0300

    Added sign_in method

--- lib/ex_mon_web/controllers/trainers_controller.ex
+++ lib/ex_mon_web/controllers/trainers_controller.ex
@@ -1,7 +1,16 @@
 defmodule ExMonWeb.TrainersController do
   use ExMonWeb, :controller
+  alias ExMonWeb.Auth.Guardian
 
   action_fallback ExMonWeb.FallbackController
+
+  def sign_in(conn, params) do
+    with {:ok, token} <- Guardian.authenticate(params) do
+      conn
+      |> put_status(:ok)
+      |> render("sign_in.json", token: token)
+    end
+  end
 
   def show(conn, %{"id" => id}) do
     id
@@ -10,9 +19,12 @@ defmodule ExMonWeb.TrainersController do
   end
 
   def create(conn, params) do
-    params
-    |> ExMon.create_trainer()
-    |> handle_response(conn, "create.json", :created)
+    with {:ok, trainer} <- ExMon.create_trainer(params),
+      {:ok, token, _claims} <- Guardian.encode_and_sign(trainer) do
+        conn
+        |> put_status(:created)
+        |> render("create.json", %{trainer: trainer, token: token})
+    end
   end
 
   def update(conn, params) do
