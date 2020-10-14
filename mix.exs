b6a47fd49775f1432d417ec498b86bef5016cc68
Pedro Rogério <pinceladasdaweb@hotmail.com>
2020-10-14 20:08:44 -0300

    Added Guardian authentication library

--- mix.exs
+++ mix.exs
@@ -44,7 +44,8 @@ defmodule ExMon.MixProject do
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
       {:argon2_elixir, "~> 2.3"},
-      {:tesla, "~> 1.3.3"}
+      {:tesla, "~> 1.3.3"},
+      {:guardian, "~> 2.0"}
     ]
   end
 
