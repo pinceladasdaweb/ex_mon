c9e6c3876003f7a947a19f8acadb1dfea521a221
Pedro Rogério <pinceladasdaweb@hotmail.com>
2020-10-14 20:26:35 -0300

    Update config for auth pipeline

--- config/config.exs
+++ config/config.exs
@@ -30,6 +30,10 @@ config :ex_mon, ExMonWeb.Auth.Guardian,
   issuer: "ex_mon",
   secret_key: System.get_env("SECRET")
 
+config :ex_mon, ExMonWeb.Auth.Pipeline,
+  module: ExMonWeb.Auth.Guardian,
+  error_handler: ExMonWeb.Auth.ErrorHandler
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
